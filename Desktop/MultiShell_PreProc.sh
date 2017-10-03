6c6762486d57b11bbbf6f41651a552682358a09a
adpines <31902603+adpines@users.noreply.github.com>
2017-10-03 13:53:54 -0400

    Update MultiShell_PreProc.sh

--- Desktop/MultiShell_PreProc.sh
+++ Desktop/MultiShell_PreProc.sh
@@ -19,35 +19,37 @@ indx=""
 for ((i=1; i<119; i+=1)); do indx="$indx 1"; done
 
 for i in $general;do 
+	bblIDs=$(echo ${i}|cut -d'/' -f8 |sed s@'/'@' '@g);
 	inputnifti=$(echo ${i}DTI_MultiShell_117dir/nifti/*.nii.gz)
 	unroundedbval=$(echo ${i}DTI_MultiShell_117dir/nifti/*.bval)
 	topupref=$(echo ${i}DTI_MultiShell_topup_ref/nifti/*.nii.gz)
 	bvec=$(echo ${i}DTI_MultiShell_117dir/nifti/*.bvec)
+	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/${bblIDs}
+	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs/QA
+	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs/Intermediate
+	out=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs
 	
 # Round bvals up or down 5, corrects for scanner output error in bvals	
-	./bval_rounder.sh $unroundedbval /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/QA/roundedbval.bval 100
+	./bval_rounder.sh $unroundedbval $out/QA/roundedbval.bval 100
 # Get quality assurance metrics on DTI data for each shell
-	~/qa_dti_v3.sh $inputnifti /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/QA/roundedbval.bval $bvec /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/QA/dwi.qa
+	~/qa_dti_v3.sh $inputnifti $out/QA/roundedbval.bval $bvec $out/QA/dwi.qa
 # Extract b0 from anterior to posterior phase-encoded input nifti for topup calculation
-	fslroi $inputnifti /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/nodif_AP 0 1
+	fslroi $inputnifti $out/Intermediates/nodif_AP 0 1
 # Extract b0 from P>A topup ref for topup calculation
-	fslroi $topupref /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/nodif_PA 0 1
+	fslroi $topupref $out/Intermediates/nodif_PA 0 1
 # Merge b0s for topup calculation
-	fslmerge -t /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/b0s /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/nodif_AP /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/nodif_PA
+	fslmerge -t $out/Intermediates/b0s $out/Intermediates/nodif_AP $out/Intermediates/nodif_PA
 # Run topup to calculate correction for field distortion
-	topup --imain=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/b0s.nii.gz --datain=$1 --out=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/my_topup --fout=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/my_field --iout=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/topup_iout
+	topup --imain=$out/Intermediates/b0s.nii.gz --datain=$1 --config=b02b0.cnf --out=$out/Intermediates/my_topup --fout=$out/Intermediates/my_field --iout=$out/Intermediates/topup_iout
 # Actually correct field distortion
-	applytopup --imain=$inputnifti --datain=$1 --inindex=1 --topup=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/my_topup --out=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/topup_applied --method=jac
+	applytopup --imain=$inputnifti --datain=$1 --inindex=1 --topup=$out/Intermediates/my_topup --out=$out/Intermediates/topup_applied --method=jac
 # Average MR signal over all volumes so brain extraction can work on signal representative of whole scan
-	fslmaths /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/topup_iout.nii.gz -Tmean /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/mean_iout.nii.gz
+	fslmaths $out/Intermediates/topup_iout.nii.gz -Tmean $out/Intermediates/mean_iout.nii.gz
 # Brain extraction mask for eddy, -m makes binary mask
-	bet /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/mean_iout.nii.gz /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/bet_iout -m
+	bet $out/Intermediates/mean_iout.nii.gz $out/Intermediates/bet_iout -m
 # Create index for eddy to know which acquisition parameters apply to which volumes.(Original usage only correcting A>P, only using one set of acq params.
 	echo $indx > index.txt
 # Run eddy correction. Corrects for Electromagnetic-pulse induced distortions. Most computationally intensive of anything here, has taken >5 hours. More recent eddy correction available in more recent FSL versions
-	/share/apps/fsl/5.0.5/bin/eddy --imain=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/topup_applied.nii.gz --mask=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/Intermediates/bet_iout.nii.gz --index=index.txt --acqp=$1 --bvecs=$bvec --bvals=roundedbval.bval --out=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/eddied
+	/share/apps/fsl/5.0.5/bin/eddy --imain=$out/Intermediates/topup_applied.nii.gz --mask=$out/Intermediates/bet_iout.nii.gz --index=index.txt --acqp=$1 --bvecs=$bvec --bvals=roundedbval.bval --out=$out/eddied
 
 done
-
-	
-	
