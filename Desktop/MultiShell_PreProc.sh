7d2ec88eef4b2f1e84d04d1b38e20c239d566640
adpines <31902603+adpines@users.noreply.github.com>
2017-10-06 15:04:40 -0400

    Update MultiShell_PreProc.sh

--- Desktop/MultiShell_PreProc.sh
+++ Desktop/MultiShell_PreProc.sh
@@ -13,7 +13,8 @@
 
 #eddy step requires more memory than default allocation of 3 G of RAM. Use at least -l h_vmem=3.5,s_vmem=3
 
-general=/data/joy/BBL/studies/grmpy/rawData/127611/*/
+general=/data/joy/BBL/studies/grmpy/rawData/*/*/
+scripts=/home/melliott/scripts
 acqp=$1
 indx=""	
 for ((i=1; i<119; i+=1)); do indx="$indx 1"; done
@@ -26,13 +27,13 @@ for i in $general;do
 	bvec=$(echo ${i}DTI_MultiShell_117dir/nifti/*.bvec)
 	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/${bblIDs}
 	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs/QA
-	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs/Intermediate
+	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs/Intermediates
 	out=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs
 	
 # Round bvals up or down 5, corrects for scanner output error in bvals	
-	./bval_rounder.sh $unroundedbval $out/QA/roundedbval.bval 100
+	$scripts/bval_rounder.sh $unroundedbval $out/QA/roundedbval.bval 100
 # Get quality assurance metrics on DTI data for each shell
-	~/qa_dti_v3.sh $inputnifti $out/QA/roundedbval.bval $bvec $out/QA/dwi.qa
+	$scripts/qa_dti_v3.sh $inputnifti $out/QA/roundedbval.bval $bvec $out/QA/dwi.qa
 # Extract b0 from anterior to posterior phase-encoded input nifti for topup calculation
 	fslroi $inputnifti $out/Intermediates/nodif_AP 0 1
 # Extract b0 from P>A topup ref for topup calculation
@@ -46,7 +47,7 @@ for i in $general;do
 # Average MR signal over all volumes so brain extraction can work on signal representative of whole scan
 	fslmaths $out/Intermediates/topup_iout.nii.gz -Tmean $out/Intermediates/mean_iout.nii.gz
 # Brain extraction mask for eddy, -m makes binary mask
-	bet $out/Intermediates/mean_iout.nii.gz $out/Intermediates/bet_iout -m
+	bet $out/Intermediates/mean_iout.nii.gz $out/Intermediates/bet_iout -m -f 0.2
 # Create index for eddy to know which acquisition parameters apply to which volumes.(Original usage only correcting A>P, only using one set of acq params.
 	echo $indx > index.txt
 # Run eddy correction. Corrects for Electromagnetic-pulse induced distortions. Most computationally intensive of anything here, has taken >5 hours. More recent eddy correction available in more recent FSL versions
