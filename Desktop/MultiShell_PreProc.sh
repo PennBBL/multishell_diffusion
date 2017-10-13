16f31c4cf196a5ed70724975fd5a4523998a4a4f
adpines <31902603+adpines@users.noreply.github.com>
2017-10-13 15:42:40 -0400

    Update MultiShell_PreProc.sh

--- Desktop/MultiShell_PreProc.sh
+++ Desktop/MultiShell_PreProc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Runs nfitis thorugh QA, topup, and Eddy. 
+#Runs nfitis thorugh QA, topup, and eddy. Calculates Diffusion space -> Structural space affine, uses this calculations and pre-existing antsCT folder affine to template, and warp to template. 
 
 #Prompted input: acquisition parameters a b c d (multiple rows for multiple phase encoding directions).
 
@@ -13,7 +13,7 @@
 
 #eddy step requires more memory than default allocation of 3 G of RAM. Use at least -l h_vmem=3.5,s_vmem=3
 
-general=/data/joy/BBL/studies/grmpy/rawData/*/*/
+general=/data/joy/BBL/studies/grmpy/rawData/110828/*/
 scripts=/home/melliott/scripts
 acqp=$1
 indx=""	
@@ -27,7 +27,8 @@ for i in $general;do
 	bvec=$(echo ${i}DTI_MultiShell_117dir/nifti/*.bvec)
 	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/${bblIDs}
 	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs/QA
-	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs/Intermediates
+	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs/Topup
+	mkdir /data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs/Transforms
 	out=/data/joy/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/$bblIDs
 	
 # Round bvals up or down 5, corrects for scanner output error in bvals	
@@ -35,22 +36,39 @@ for i in $general;do
 # Get quality assurance metrics on DTI data for each shell
 	$scripts/qa_dti_v3.sh $inputnifti $out/QA/roundedbval.bval $bvec $out/QA/dwi.qa
 # Extract b0 from anterior to posterior phase-encoded input nifti for topup calculation	
-	fslroi $inputnifti $out/Intermediates/nodif_AP 0 1
+	fslroi $inputnifti $out/Topup/nodif_AP 0 1
 # Extract b0 from P>A topup ref for topup calculation
-	fslroi $topupref $out/Intermediates/nodif_PA 0 1
+	fslroi $topupref $out/Topup/nodif_PA 0 1
 # Merge b0s for topup calculation
-	fslmerge -t $out/Intermediates/b0s $out/Intermediates/nodif_AP $out/Intermediates/nodif_PA
+	fslmerge -t $out/Topup/b0s $out/Topup/nodif_AP $out/Topup/nodif_PA
 # Run topup to calculate correction for field distortion
-	topup --imain=$out/Intermediates/b0s.nii.gz --datain=$1 --config=b02b0.cnf --out=$out/Intermediates/my_topup --fout=$out/Intermediates/my_field --iout=$out/Intermediates/topup_iout
+	topup --imain=$out/Topup/b0s.nii.gz --datain=$1 --config=b02b0.cnf --out=$out/Topup/my_topup --fout=$out/Topup/my_field --iout=$out/Topup/topup_iout
 # Actually correct field distortion
-	applytopup --imain=$inputnifti --datain=$1 --inindex=1 --topup=$out/Intermediates/my_topup --out=$out/Intermediates/topup_applied --method=jac
+	applytopup --imain=$inputnifti --datain=$1 --inindex=1 --topup=$out/Topup/my_topup --out=$out/Topup/topup_applied --method=jac
 # Average MR signal over all volumes so brain extraction can work on signal representative of whole scan
-	fslmaths $out/Intermediates/topup_iout.nii.gz -Tmean $out/Intermediates/mean_iout.nii.gz
+	fslmaths $out/Topup/topup_iout.nii.gz -Tmean $out/Topup/mean_iout.nii.gz
 # Brain extraction mask for eddy, -m makes binary mask
-	bet $out/Intermediates/mean_iout.nii.gz $out/Intermediates/bet_iout -m -f 0.2
+	bet $out/Topup/mean_iout.nii.gz $out/Topup/bet_iout_point_2 -m -f 0.2
 # Create index for eddy to know which acquisition parameters apply to which volumes.(Original usage only correcting A>P, only using one set of acq params.
 	echo $indx > index.txt
 # Run eddy correction. Corrects for Electromagnetic-pulse induced distortions. Most computationally intensive of anything here, has taken >5 hours. More recent eddy correction available in more recent FSL versions
-	/share/apps/fsl/5.0.5/bin/eddy --imain=$out/Intermediates/topup_applied.nii.gz --mask=$out/Intermediates/bet_iout.nii.gz --index=index.txt --acqp=$1 --bvecs=$bvec --bvals=$out/QA/roundedbval.bval --out=$out/eddied
+	/share/apps/fsl/5.0.5/bin/eddy --imain=$out/Topup/topup_applied.nii.gz --mask=$out/Topup/bet_iout_point_2.nii.gz --index=index.txt --acqp=$1 --bvecs=$bvec --bvals=$out/QA/roundedbval.bval --out=$out/eddied
+
+# re-bet eddy output
+	bet $out/eddied_point_2.nii.gz $out/eddied_bet_2.nii.gz -f 0.2
+
+# make white matter only mask from segmented T1 in prep for flirt BBR
+        fslmaths /data/joy/BBL/studies/grmpy/processedData/structural/struct_pipeline_20170716/$bblIDs/*/antsCT/*_BrainSegmentation.nii.gz -thr 3 -uthr 3 $out/Transforms/Struct_WM.nii.gz
+
+# use flirt to calculate diffusion -> structural translation 
+	flirt -cost bbr -wmseg $out/Transforms/Struct_WM.nii.gz -in $out/eddied_bet_2.nii.gz -ref /data/joy/BBL/studies/grmpy/processedData/structural/struct_pipeline_20170716/$bblIDs/*/antsCT/*ExtractedBrain0N4.nii.gz -out $out/Transforms/flirt_BBR -dof 6 -omat $out/Transforms/MultiShDiff2StructFSL.mat
+
+# Convert FSL omat to Ras
+	c3d_affine_tool -src $out/eddied_bet_2.nii.gz -ref /data/joy/BBL/studies/grmpy/processedData/structural/struct_pipeline_20170716/$bblIDs/*/antsCT/*ExtractedBrain0N4.nii.gz $out/Transforms/MultiShDiff2StructFSL.mat -fsl2ras -oitk $out/Transforms/MultiShDiff2StructRas.mat
+
+# Use Subject to template warp and affine from grmpy directory after Ras diffusion -> structural space affine to put eddied_bet_2 onto pnc template
+	antsApplyTransforms -e 3 -d 3 -i $out/eddied_bet_2.nii.gz -r /data/joy/BBL/studies/pnc/template/pnc_template_brain.nii.gz -o $out/Transforms/eddied_b0_template_space.nii.gz -t /data/joy/BBL/studies/grmpy/processedData/structural/struct_pipeline_20170716/$bblIDs/*/antsCT/*SubjectToTemplate1Warp.nii.gz -t /data/joy/BBL/studies/grmpy/processedData/structural/struct_pipeline_20170716/$bblIDs/*/antsCT/*SubjectToTemplate0GenericAffine.mat -t $out/Transforms/MultiShDiff2StructRas.mat
 
 done
+
+	
