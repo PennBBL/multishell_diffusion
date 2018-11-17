33ceb26d16307033a6bd04dcfda187d6d0e6ab30
adpines <pinesa@pennmedicine.upenn.edu>
2018-11-17 12:47:43 -0500

    All cleaned up

--- cpbackup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp -r /data/jux/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/ /data/jux/BBL/projects/multishell_diffusion/processedData/backup/
--- partcoefwrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ci=csvread('/data/jux/BBL/projects/pncBaumDti/Schaefer200_Yeo7_affil.txt')   
-for i in ~/torun.txt
-
-matlab -nodisplay;W=csvread('90683_20160508x10142_ICVF_matrixts.csv',1,0);cd '/data/jux/BBL/projects/multishell_diffusion/multishell_diffusionScripts/';participation_coef(W,ci);
-
