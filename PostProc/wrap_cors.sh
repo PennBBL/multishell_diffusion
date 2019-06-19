272188edc483b815041d18540cb72a532cc56b4f
adpines <pinesa@pennmedicine.upenn.edu>
2019-06-19 15:57:50 -0400

    added native-space correlations for all metrics of interest (FA,MD,RD,AD,ICVF,ISOVF,ODI,RTOP,RTPP,RTAP)

---
+++ PostProc/wrap_cors.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+general=/data/joy/BBL/tutorials/exampleData/AMICO_NODDI/raw/*/*
+
+for i in $general;do 
+	bblIDs=$(echo ${i}|cut -d'/' -f9 |sed s@'/'@' '@g);
+	SubDate_and_ID=$(echo ${i}|cut -d'/' -f10|sed s@'/'@' '@g|sed s@'x'@'x'@g)
+ 	
+		## Execute qsub job for mapl for each subject
+		echo ${bblIDs}
+		echo ${SubDate_and_ID}
+		logDir=/data/jux/BBL/projects/multishell_diffusion/processedData/multishellPipelineFall2017/${bblIDs}/${SubDate_and_ID}/logfiles
+
+		qsub -S /data/jux/BBL/projects/multishell_diffusion/envs/mapl/bin/python3 /data/joy/BBL/tutorials/code/multishell_diffusion/PostProc/correlate_scalars.py ${bblIDs} ${SubDate_and_ID}
+
+		# append was causing overwriting
+		sleep 6s
+done
